9e9d2f97048b633c8919b75f9ea7f468bbd61d76
gladys1230 <kitchan3495@gmail.com>
2019-12-18 18:46:55 -0600

    first commit

---
+++ src/components/Artist.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Artist = ({ artist }) => {
+
+  if(!artist) return null;
+
+  const { images, name, followers, genres } = artist;
+
+
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>{followers.total} followers</p>
+      <p>{genres.join(',')}</p>
+      <img 
+        src={images[0] && images[0].url} 
+        alt='artist-profile' 
+        style={{
+          widith: 200,
+          height: 200,
+          borderRadius: 100,
+          objectField: 'cover'
+        }}
+          />
+    </div>
+  )
+}
+export default Artist;
+
+
